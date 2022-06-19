59d81febca495ab4b357b15b6ea733fb9afc56f3
kuro <>
2022-06-19 16:20:29 +0900

    add helper type

--- src/helpers/type.ts
+++ src/helpers/type.ts
@@ -1,1 +1,5 @@
 export type ArrayElement<A> = A extends readonly (infer T)[] ? T : never;
+
+export type WithRequired<T, F extends keyof T> = Omit<T, F> & Required<Pick<T, F>>;
+
+export type WithOptional<T, TRequired extends keyof T> = Partial<T> & Pick<T, TRequired>;
