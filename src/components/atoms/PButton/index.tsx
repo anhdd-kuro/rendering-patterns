423c86817eac7669dd81a70d468e67f8bda9ac9f
kuro <>
2022-06-19 14:29:58 +0900

    Refactor Button atoms using style helper

--- src/components/atoms/PButton/index.tsx
+++ src/components/atoms/PButton/index.tsx
@@ -1,30 +1,36 @@
+import { BackgroundClass } from "tailwind.types";
+import { Style } from "@/helpers";
+
 type Props = {
   disabled?: boolean;
   rounded?: boolean;
   bordered?: boolean;
   children?: React.ReactNode;
   onClick?: void;
+  bgClass?: BackgroundClass;
 };
 
 const disabledStyles = "opacity-60 cursor-not-allowed";
-const borderedStyles = "border border-gray";
+const borderedStyles = "border border-gray-300";
 const roundedStyles = "rounded";
 
 const PButton: React.FC<Props> = ({
   children,
   disabled = false,
-  bordered = false,
+  bordered = true,
   rounded = false,
   onClick,
+  bgClass,
 }) => {
   return (
     <button
-      className={[
+      className={Style.joinClass([
         "flex-center",
-        `${disabled ? disabledStyles : ""}`,
-        `${bordered ? borderedStyles : ""}`,
-        `${rounded ? roundedStyles : ""}`,
-      ].join(" ")}
+        `${disabled && disabledStyles}`,
+        `${bordered && borderedStyles}`,
+        `${rounded && roundedStyles}`,
+        bgClass,
+      ])}
       disabled={disabled}
       onClick={() => onClick}
     >
