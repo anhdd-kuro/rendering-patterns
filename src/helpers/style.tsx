35e14f41e83744d589e75e643977d1da443c3df7
kuro <>
2022-06-21 22:49:44 +0900

    refactor remove styled-tw

--- src/helpers/style.tsx
+++ src/helpers/style.tsx
@@ -1,26 +1,3 @@
-import tw from "tailwind-styled-components";
 import clsx from "clsx";
 
-const twStyledFactory = <T,>(
-  Component: React.FC<T> | HTMLElement,
-  props: T,
-  className?: string,
-) => {
-  const StyledTailwind = tw(Component)`${(props: T & { className?: string }) => props.className}`;
-
-  return <StyledTailwind {...props} className={className} />;
-};
-
-const StyledTailwind = <Props,>({
-  component,
-  className,
-  props,
-}: {
-  component: React.FC<Props> | HTMLElement;
-  props: Props;
-  className?: string;
-}) => {
-  return twStyledFactory(component, props, className);
-};
-
-export { StyledTailwind, clsx, twStyledFactory };
+export { clsx };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ const defaultColors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/helper/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     colors: defaultColors,
   },
