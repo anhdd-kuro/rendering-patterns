dd81409552f289160e7dbbc60a1c831be40daeff
kuro <>
2022-06-21 16:04:19 +0900

    heading component refactor

--- src/helpers/type.ts
+++ src/helpers/type.ts
@@ -3,3 +3,5 @@ export type ArrayElement<A> = A extends readonly (infer T)[] ? T : never;
 export type WithRequired<T, F extends keyof T> = Omit<T, F> & Required<Pick<T, F>>;
 
 export type WithOptional<T, TRequired extends keyof T> = Partial<T> & Pick<T, TRequired>;
+
+export type HeadingTag = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
