d59ef0a6c12015be687aaee29d868ce43bf106ac
kuro <>
2022-06-25 22:41:44 +0900

    PNews Refactor

--- src/components/molecules/PNews/index.tsx
+++ src/components/molecules/PNews/index.tsx
@@ -1,28 +1,19 @@
 import { PLink } from "@/components/atoms";
 import { Date } from "@/helpers";
 
-export type Props = {
-  news: {
-    date: Date;
-    text: string;
-    url: string;
-  };
+type Props = {
+  date: Date;
+  text: string;
+  url: string;
 };
 
-export const PNews: React.FC<Props> = ({ news }) => {
+export const PNews: React.FC<Props> = ({ date, url, text }) => {
   return (
-    news && (
-      <div className="flex items-center">
-        <p className="mr-5 text-gray-400 text-sm">{Date.toString(news.date, "yyyy-mm-dd")}</p>
-        <PLink
-          fontSizeClass="text-lg"
-          textColorClass="text-blue-800"
-          href={news.url}
-          isExternal={false}
-        >
-          {news.text}
-        </PLink>
-      </div>
-    )
+    <div className="flex items-center">
+      <p className="mr-5 text-gray-400 text-sm">{Date.toString(date, "yyyy-mm-dd")}</p>
+      <PLink fontSizeClass="text-lg" textColorClass="text-blue-800" href={url} isExternal={false}>
+        {text}
+      </PLink>
+    </div>
   );
 };
