28d9f09210afa3dde7b53b00147c61b598ff6458
kuro <>
2022-06-26 19:01:54 +0900

    Home page with mock api

---
+++ src/infra/axiosClient/index.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const axiosClient = axios.create({
+  baseURL: process.env.API_SERVER,
+  headers: {
+    "content-type": "application/json",
+  },
+  // paramsSerializer: (params) => queryString.stringify(params),
+});
+
+axiosClient.interceptors.request.use(
+  function (config) {
+    // Do something before request is sent
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  },
+);
+
+axiosClient.interceptors.response.use(
+  function (response) {
+    if (response && response.data) {
+      return response.data;
+    }
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  },
+);
+
+export default axiosClient;
