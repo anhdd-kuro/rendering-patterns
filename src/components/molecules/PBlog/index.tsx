16767e6c153a30107b6067b3a77aa2f0c1224b17
kuro <>
2022-06-27 14:12:54 +0900

    Blog component refactor

--- src/components/molecules/PBlog/index.tsx
+++ src/components/molecules/PBlog/index.tsx
@@ -2,51 +2,44 @@ import { PHeading, PImage, PLink, PTag } from "@/components/atoms";
 import { Date, Type, Style } from "@/helpers";
 
 type Props = {
-  url: string;
-  thumbnail: {
-    src: string;
-    alt: string;
+  data: {
+    url: string;
+    thumbnail: {
+      src: string;
+      alt: string;
+    };
+    date: Date;
+    category: string;
+    title: string;
+    tags: string[];
   };
-  date: Date;
-  category: string;
-  title: string;
-  tags: string[];
   heading: Type.HeadingTag;
   creditContent: React.ReactNode;
 };
 
-export const PBlog: React.FC<Props> = ({
-  url,
-  thumbnail,
-  title,
-  date,
-  category,
-  heading,
-  tags,
-  creditContent,
-}) => {
+export const PBlog: React.FC<Props> = ({ data, heading, creditContent }) => {
   return (
     <figure className="relative flex justify-between">
       <div role="img" className="relative w-32 h-32 overflow-hidden mr-10">
-        <PLink href={url} isExternal={false}>
-          <PImage src={thumbnail.src} alt={thumbnail.alt || `${title}の画像`} />
+        <PLink href={data.url} isExternal={false}>
+          <PImage src={data.thumbnail.src} alt={data.thumbnail.alt || `${data.title}の画像`} />
         </PLink>
       </div>
       <figcaption className={Style.clsx("relative flex-1 flex flex-col gap-3")}>
         <div className="flex items-center leading-none text-[0.8em]">
-          <p className="text-gray-500 font-bold">{Date.toString(date, "yyyy-mm-dd")}</p>
+          <p className="text-gray-500 font-bold">{Date.toString(data.date, "yyyy-mm-dd")}</p>
           <p
             className="ml-1 mb-[0.3em] px-1"
             style={{
               background: "linear-gradient(180deg, #fff, #fff 56%, #f6d322 57%, #f6d322)",
             }}
           >
-            {category}
+            {data.category}
           </p>
         </div>
-        <PHeading text={title} tag={heading} fontSizeClass="text-2xl" />
+        <PHeading text={data.title} tag={heading} fontSizeClass="text-2xl" />
         <div className="space-x-2">
-          {tags.map((tag, index) => (
+          {data.tags.map((tag, index) => (
             <PTag label={tag} key={index} fontSizeClass="text-xs" />
           ))}
         </div>
