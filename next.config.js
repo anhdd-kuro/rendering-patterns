7bf17e9cd60d28cc28d9f6e76a7043bc75d5cdb0
kuro <>
2022-06-27 11:02:20 +0900

    Add gql loader

--- next.config.js
+++ next.config.js
@@ -4,6 +4,17 @@ const nextConfig = {
   env: {
     API_SERVER: process.env.API_SERVER,
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(graphql|gql)$/,
+      exclude: /node_modules/,
+      loader: "graphql-tag/loader",
+    });
+    return config;
+  },
+  webpackDevMiddleware: (config) => {
+    return config;
+  },
 };
 
 module.exports = nextConfig;
