35962eb73b94d05848a8ca078a1e48b6b8759120
kuro <>
2022-06-27 16:13:48 +0900

    PHome template refactor

--- src/components/templates/PHome/index.tsx
+++ src/components/templates/PHome/index.tsx
@@ -26,22 +26,23 @@ export const PHome: React.FC<Props> = ({
       <section
         id="blog"
         className={Style.clsx(
-          "flex flex-col",
-          "border-yellow-300 border-8 py-[100px] px-[200px] space-y-20",
+          "flex flex-col items-center",
+          "border-yellow-300 border-8 py-[100px] px-[20px]",
         )}
       >
-        {blogSectionTitle || <h2>Blogs Title</h2>}
-        {blogsSectionContent || <p>Blogs Content</p>}
+        <div className="max-w-[1000px] w-full space-y-20">
+          {blogSectionTitle || <h2>Blogs Title</h2>}
+          {blogsSectionContent || <p>Blogs Content</p>}
+        </div>
       </section>
       <section
         id="members"
-        className={Style.clsx(
-          "flex flex-col",
-          "border-blue-600 border-8 py-[100px] px-[200px] space-y-20",
-        )}
+        className={Style.clsx("flex flex-col", "border-blue-600 border-8 py-[100px] px-[200px] ")}
       >
-        {membersSectionTitle || <h2>Members Title</h2>}
-        {membersSectionContent || <p>Members Content</p>}
+        <div className="max-w-[1000px] w-full space-y-20">
+          {membersSectionTitle || <h2>Members Title</h2>}
+          {membersSectionContent || <p>Members Content</p>}
+        </div>
       </section>
     </>
   );
