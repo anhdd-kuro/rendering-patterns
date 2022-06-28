da840db0b36652d3e3e3dc206a06d8496dfc6c7b
kuro <>
2022-06-28 17:27:06 +0900

    add type helper

--- src/helpers/type.ts
+++ src/helpers/type.ts
@@ -5,3 +5,5 @@ export type WithRequired<T, F extends keyof T> = Omit<T, F> & Required<Pick<T, F
 export type WithOptional<T, TRequired extends keyof T> = Partial<T> & Pick<T, TRequired>;
 
 export type HeadingTag = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+
+export type ValueOf<T> = T[keyof T];
