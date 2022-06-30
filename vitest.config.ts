82ea6dd9e644793f0c98d92e180f640fd155999e
kuro <>
2022-06-30 17:21:08 +0900

    Remove unnesesary setting

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,15 +10,4 @@ export default defineConfig({
     globals: true,
     environment: "happy-dom",
   },
-  resolve: {
-    alias: {
-      /**
-       * Storybook (specifically the interactions addon) requires that we use their
-       *   instrumented version of jest-expect. So our storybook does so. To make
-       *   these interactions still work in vitest we have @storybook/jest aliased
-       *   to resolve to vitest which, critically, exports { expect } as well.
-       */
-      "@storybook/jest": "vitest",
-    },
-  },
 });
