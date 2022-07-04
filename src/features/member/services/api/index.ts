9a90927a4a445f9b916d9a2f235236f279c5ce2e
kuro <>
2022-07-04 18:43:31 +0900

    Fix import

--- src/features/member/services/api/index.ts
+++ src/features/member/services/api/index.ts
@@ -1,10 +1,10 @@
-import { graphQLClient, Member } from "@/infra/graphql";
+import { GraphQl } from "@/infra";
 import { MemberInputSchema, GetAllMembers } from "@/features/member/domain";
 
 import { z } from "zod";
 
-export const getAllMembers = async (): Promise<Member[]> => {
-  const { members } = await graphQLClient.request(GetAllMembers);
+export const getAllMembers = async (): Promise<GraphQl.Member[]> => {
+  const { members } = await GraphQl.graphQLClient.request(GetAllMembers);
 
   const validatedData = MemberInputSchema().extend({ id: z.string() }).array().parse(members);
 
